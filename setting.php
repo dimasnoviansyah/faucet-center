53faffbe5a14961d78c2114dd2011fb63fa92031
dimasnoviansyah <mayui64bit@gmail.com>
2015-10-07 04:54:24 +0700

    Create setting.php

---
+++ setting.php
@@ -0,0 +1,23 @@
+<?php
+
+class SystemComponent{
+
+	private $settings;
+
+	function getSetting(){
+
+			$settings['dbusername']='root';
+
+			$settings['dbpassword']=''; 
+
+			$settings['dbname']='asmoney';
+
+			$settings['dbhost']='localhost';
+
+			return $settings;
+
+	}
+
+}
+
+?>
